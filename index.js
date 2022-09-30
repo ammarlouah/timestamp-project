a8715f3dcb6f11d7827235018d35252b17e8d67c
Ammar <ammarlouah9@gmail.com>
2022-09-30 13:20:09 +0100

    preparing app for production and deploying it to heroku

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 // where your node app starts
 
 // init project
+
 require('dotenv').config();
 var express = require('express');
 var app = express();
+require('./startup/prod')(app);
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
